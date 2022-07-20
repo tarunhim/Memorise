892172fb6b5e6475b7c14ab832db75854e1f315f
Tarun kumar chourasiya <tkcthousand@gmail.com>
2022-07-20 14:54:42 +0530

    Updates

---
+++ precache-manifest.b4df07b400aca991e93fc3dcdf7feeda.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b80d5c562855ba6de63c023cc18ebb0c",
+    "url": "/Memorise/index.html"
+  },
+  {
+    "revision": "4d0a5d6e47efa48d06ca",
+    "url": "/Memorise/static/css/main.4e89baf1.chunk.css"
+  },
+  {
+    "revision": "20ae1808e1c9af56956c",
+    "url": "/Memorise/static/js/2.bfb52602.chunk.js"
+  },
+  {
+    "revision": "90377550dd86ed303dd30d8f487872de",
+    "url": "/Memorise/static/js/2.bfb52602.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d0a5d6e47efa48d06ca",
+    "url": "/Memorise/static/js/main.c3427d7b.chunk.js"
+  },
+  {
+    "revision": "60d57d53d29abca8095c",
+    "url": "/Memorise/static/js/runtime-main.e2898091.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/Memorise/static/media/memories.5c0c63fb.png"
+  }
+]);
