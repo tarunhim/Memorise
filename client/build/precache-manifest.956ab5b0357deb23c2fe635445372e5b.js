b293876155df40f423b66aa9f43e6364cce2e9c5
Tarun kumar chourasiya <tkcthousand@gmail.com>
2022-07-23 00:37:31 +0530

    my first full stack mern project

---
+++ client/build/precache-manifest.956ab5b0357deb23c2fe635445372e5b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3babdfc54a8d31e86e332f3d919774a0",
+    "url": "/Memorise/index.html"
+  },
+  {
+    "revision": "51eaf3e7da150e20fa80",
+    "url": "/Memorise/static/css/main.4e89baf1.chunk.css"
+  },
+  {
+    "revision": "20ae1808e1c9af56956c",
+    "url": "/Memorise/static/js/2.bfb52602.chunk.js"
+  },
+  {
+    "revision": "90377550dd86ed303dd30d8f487872de",
+    "url": "/Memorise/static/js/2.bfb52602.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51eaf3e7da150e20fa80",
+    "url": "/Memorise/static/js/main.99ecaaa4.chunk.js"
+  },
+  {
+    "revision": "60d57d53d29abca8095c",
+    "url": "/Memorise/static/js/runtime-main.e2898091.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/Memorise/static/media/memories.5c0c63fb.png"
+  }
+]);
